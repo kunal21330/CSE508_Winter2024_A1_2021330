d7d57283388de0697b18c92586c1d1f622c54036
kunal21330 <kunal21330@iiitd.ac.in>
2024-02-08 19:39:43 +0530

    added tokenization,remomved stop_words.spaces and punctuation from data

--- preprocessing.py
+++ preprocessing.py
@@ -1,7 +1,15 @@
 import os
+import string
 import nltk
+from nltk.corpus import stopwords
+
+
+nltk.download('punkt')
 
 dataset_path='text_files'
+stop_words = set(stopwords.words('english'))
+
+translator = str.maketrans(' ',' ',string.punctuation)
 
 for file in os.listdir(dataset_path):
     if file.endswith('.txt'):
@@ -10,7 +18,11 @@ for file in os.listdir(dataset_path):
         raw_data=nltk.data.load(file_path,format='text')
 
         lowercase=raw_data.lower()
+        tokenized=nltk.word_tokenize(lowercase)
+        filtered_tokens = [token for token in tokenized if token not in stop_words]
+        removepanctuation=[word.translate(translator) for word in filtered_tokens]
+
 
         Preprocessed_file_path = os.path.join(dataset_path,'preprocessed_'+ file)
         with open(Preprocessed_file_path,'w') as f:
-            f.write(raw_data)
+            f.write(' '.join(removepanctuation))
