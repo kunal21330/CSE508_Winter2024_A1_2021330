3d5e101a7f6ba644427c81259b1c2b474d29e2bb
kunal21330 <kunal21330@iiitd.ac.in>
2024-02-09 17:13:27 +0530

    made different folder for preprocessed files

--- preprocessing.py
+++ preprocessing.py
@@ -9,20 +9,28 @@ nltk.download('punkt')
 dataset_path='text_files'
 stop_words = set(stopwords.words('english'))
 
-translator = str.maketrans(' ',' ',string.punctuation)
+translator = str.maketrans('','',string.punctuation)
 
 for file in os.listdir(dataset_path):
     if file.endswith('.txt'):
         file_path=os.path.join(dataset_path,file)
 
+
         raw_data=nltk.data.load(file_path,format='text')
 
+
         lowercase=raw_data.lower()
+
+
         tokenized=nltk.word_tokenize(lowercase)
+
+
         filtered_tokens = [token for token in tokenized if token not in stop_words]
+
+
         removepanctuation=[word.translate(translator) for word in filtered_tokens]
 
 
-        Preprocessed_file_path = os.path.join(dataset_path,'preprocessed_'+ file)
+        Preprocessed_file_path = os.path.join('preprocessed_files','preprocessed_'+ file)
         with open(Preprocessed_file_path,'w') as f:
             f.write(' '.join(removepanctuation))
